7273927ee1f95ca042173875c17499f439dddc16
RiccardoBusetti <riccardob36@gmail.com>
2019-11-12 11:44:38 +0100

    Add support for dark mode in the webview

--- app/src/main/java/com/riccardobusetti/unibztimetable/ui/canteen/CanteenFragment.kt
+++ app/src/main/java/com/riccardobusetti/unibztimetable/ui/canteen/CanteenFragment.kt
@@ -1,15 +1,19 @@
 package com.riccardobusetti.unibztimetable.ui.canteen
 
 import android.annotation.SuppressLint
+import android.content.res.Configuration.*
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import com.riccardobusetti.unibztimetable.R
 import com.riccardobusetti.unibztimetable.utils.CanteenWebViewClient
 import kotlinx.android.synthetic.main.fragment_canteen.*
+
 
 class CanteenFragment : Fragment() {
 
@@ -33,6 +37,15 @@ class CanteenFragment : Fragment() {
     private fun setupUi() {
         webView = fragment_canteen_web_view
         webView.settings.javaScriptEnabled = true
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val nightModeFlags = resources.configuration.uiMode and UI_MODE_NIGHT_MASK
+
+            if (nightModeFlags == UI_MODE_NIGHT_YES) {
+                webView.settings.forceDark = WebSettings.FORCE_DARK_ON
+            } else if (nightModeFlags == UI_MODE_NIGHT_NO) {
+                webView.settings.forceDark = WebSettings.FORCE_DARK_OFF
+            }
+        }
         webView.webViewClient = CanteenWebViewClient(context!!)
 
         webView.loadUrl(CanteenWebViewClient.CANTEEN_URL)
